b4aa3572c0c91c8de789dbd461bba6297716b105
Lars C. Finlay <larsfinlay@googlemail.com>
2021-03-23 11:31:49 +0000

    VideoGames#get_all red

--- ruby/sinatra/spec/services/video_games_spec.rb
+++ ruby/sinatra/spec/services/video_games_spec.rb
@@ -5,4 +5,18 @@ require_relative '../../lib/services/video_games'
 describe VideoGames do
     let(:storage) { double 'mock storage' }
     subject { VideoGames.new storage }
+
+    describe '#get_all' do
+        it 'should get_all video games from storage and return them' do
+            # Arrange
+            video_games = []
+            allow(storage).to receive(:get_all).and_return video_games
+
+            # Act
+            retrieved_video_games = subject.get_all
+
+            # Assert
+            expect(retrieved_video_games).to be video_games
+        end
+    end
 end
