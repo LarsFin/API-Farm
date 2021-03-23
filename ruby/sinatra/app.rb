4db83c97cdac258a9d3a2f8e1129389612fb1d70
Lars C. Finlay <larsfinlay@googlemail.com>
2021-03-23 12:03:05 +0000

    get_all request success

--- ruby/sinatra/app.rb
+++ ruby/sinatra/app.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
+require 'json'
 require 'sinatra'
+
+require_relative 'lib/services/video_games'
+require_relative 'lib/storage/in_memory'
+
+storage = InMemory.new
+service = VideoGames.new storage
 
 get '/' do
     'Hello World!'
+end
+
+get '/video_games' do
+    service.get_all.to_json
 end
