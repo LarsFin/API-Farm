862710859f7ac0f7287bb9a4b01736e10725008e
Lars C. Finlay <larsfinlay@googlemail.com>
2021-03-23 12:15:21 +0000

    VideoGames#get_all (json) red

--- ruby/sinatra/spec/services/video_games_spec.rb
+++ ruby/sinatra/spec/services/video_games_spec.rb
@@ -10,13 +10,15 @@ describe VideoGames do
         it 'should get_all video games from storage and return them' do
             # Arrange
             video_games = []
+            video_games_json = 'video games as json'
             allow(storage).to receive(:get_all).and_return video_games
+            allow(video_games).to receive(:to_json).and_return video_games_json
 
             # Act
             retrieved_video_games = subject.get_all
 
             # Assert
-            expect(retrieved_video_games).to be video_games
+            expect(retrieved_video_games).to be video_games_json
         end
     end
 end
