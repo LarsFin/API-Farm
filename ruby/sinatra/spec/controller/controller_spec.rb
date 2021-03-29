e9b7d455fba91d8aa829951afcedfc6dcbeb4e9c
Lars C. Finlay <larsfinlay@googlemail.com>
2021-03-29 15:14:33 +0100

    controller#get_all_video_games (with json) red

--- ruby/sinatra/spec/controller/controller_spec.rb
+++ ruby/sinatra/spec/controller/controller_spec.rb
@@ -10,16 +10,18 @@ describe Controller do
         it 'should return correct response with 200 status code' do
             # Arrange
             video_games = double 'video games'
+            json_video_games = double 'video games as json'
             allow(video_games_service).to receive(:get_all).and_return video_games
+            allow(video_games).to receive(:to_json).and_return json_video_games
 
             # Act
             response = subject.get_all_video_games
 
             # Assert
             expect(response[0]).to eq 200
-            expected_headers = { 'Content-Type': 'application/json' }
+            expected_headers = { 'Content-Type' => 'application/json' }
             expect(response[1]).to eq expected_headers
-            expect(response[2]).to be video_games
+            expect(response[2]).to be json_video_games
         end
     end
 end
