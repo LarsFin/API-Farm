79a0c6e119591ea76f3407d64f0dea255a0801f9
Lars C. Finlay <larsfinlay@googlemail.com>
2021-03-29 10:22:26 +0100

    controller#get_all_video_games refactor

--- ruby/sinatra/lib/controller/controller.rb
+++ ruby/sinatra/lib/controller/controller.rb
@@ -2,16 +2,16 @@
 
 # couples sinatra framework with services
 class Controller
-    def initialize video_games_service
+    def initialize(video_games_service)
         @video_games_service = video_games_service
     end
 
     def get_all_video_games
         video_games = @video_games_service.get_all
         [
-            200,
-            { 'Content-Type': 'application/json' },
-            video_games
+          200,
+          { 'Content-Type': 'application/json' },
+          video_games
         ]
     end
 end
