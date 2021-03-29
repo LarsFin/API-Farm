7803fb5fe111a488497924661cc6d45fb384c285
Lars C. Finlay <larsfinlay@googlemail.com>
2021-03-29 15:18:29 +0100

    controller#get_all_video_games (with json) green

--- ruby/sinatra/lib/controller/controller.rb
+++ ruby/sinatra/lib/controller/controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'json'
+
 # couples sinatra framework with services
 class Controller
     def initialize(video_games_service)
@@ -8,10 +10,11 @@ class Controller
 
     def get_all_video_games
         video_games = @video_games_service.get_all
+
         [
           200,
           { 'Content-Type' => 'application/json' },
-          video_games
+          video_games.to_json
         ]
     end
 end
