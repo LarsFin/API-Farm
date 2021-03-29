e0cabebd5e4a9c78dd3d991e9b4fb10ca6aaf4a7
Lars C. Finlay <larsfinlay@googlemail.com>
2021-03-29 16:04:27 +0100

    controller#add_video_game green

--- ruby/sinatra/lib/controller/controller.rb
+++ ruby/sinatra/lib/controller/controller.rb
@@ -17,4 +17,23 @@ class Controller
           video_games.to_json
         ]
     end
+
+    def add_video_game request
+        video_game_data = JSON.parse request.body.read
+        addition = @video_games_service.add video_game_data
+
+        if addition.fail_reason
+            [
+                400,
+                { 'Content-Type' => 'text/plain' },
+                addition.fail_reason
+            ]
+        else
+            [
+                201,
+                { 'Content-Type' => 'application/json' },
+                addition.result.to_json
+            ]
+        end
+    end
 end
