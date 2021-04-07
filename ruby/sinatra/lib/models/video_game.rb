8f55e89861219ef997a20225fe6f90a6dda24485
Lars C. Finlay <larsfinlay@googlemail.com>
2021-04-07 10:54:10 +0100

    video_game#to_hash green

--- ruby/sinatra/lib/models/video_game.rb
+++ ruby/sinatra/lib/models/video_game.rb
@@ -13,4 +13,21 @@ class VideoGame
                   :composers,
                   :platforms,
                   :date_released
+
+    def to_hash
+        {
+            'id' => @id,
+            'name' => @name,
+            'developers' => @developers,
+            'publishers' => @publishers,
+            'directors' => @directors,
+            'producers' => @producers,
+            'designers' => @designers,
+            'programmers' => @programmers,
+            'artists' => @artists,
+            'composers' => @composers,
+            'platforms' => @platforms,
+            'date_released' => @date_released
+        }
+    end
 end
