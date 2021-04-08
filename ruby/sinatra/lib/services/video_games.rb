ec24ea0a921a9cea939cee44f60ffad94726cc86
Lars C. Finlay <larsfinlay@googlemail.com>
2021-04-08 10:49:14 +0100

    video_games#add (srp) refactor

--- ruby/sinatra/lib/services/video_games.rb
+++ ruby/sinatra/lib/services/video_games.rb
@@ -16,7 +16,7 @@ class VideoGames
     def add(video_game_data)
         video_game = @video_game_class.new
 
-        fail_result = set_name_and_date video_game, video_game_data
+        fail_result = validate_name_and_date video_game, video_game_data
 
         return fail_result if fail_result
 
@@ -44,18 +44,16 @@ class VideoGames
 
   private
 
-    def set_name_and_date(video_game, video_game_data)
+    def validate_name_and_date(video_game, video_game_data)
         return { fail_reason: 'A name is required for a video game.' } unless video_game_data['name']
 
-        video_game.name = video_game_data['name']
+        set_name video_game, video_game_data
 
         return { fail_reason: 'A date_released is required for a video game.' } unless video_game_data['date_released']
 
-        begin
-            video_game.date_released = Date.parse video_game_data['date_released']
-        rescue StandardError
-            return { fail_reason: "The provided date_released '#{video_game_data['date_released']}' is invalid." }
-        end
+        fail_result = set_date video_game, video_game_data
+
+        return fail_result if fail_result
 
         nil
     end
@@ -72,39 +70,55 @@ class VideoGames
         set_platforms video_game, video_game_data
     end
 
+    def set_name(video_game, video_game_data)
+        video_game.name = video_game_data['name'] if video_game_data['name']
+    end
+
     def set_developers(video_game, video_game_data)
-        video_game.developers = video_game_data['developers']
+        video_game.developers = video_game_data['developers'] if video_game_data['developers']
     end
 
     def set_publishers(video_game, video_game_data)
-        video_game.publishers = video_game_data['publishers']
+        video_game.publishers = video_game_data['publishers'] if video_game_data['publishers']
     end
 
     def set_directors(video_game, video_game_data)
-        video_game.directors = video_game_data['directors']
+        video_game.directors = video_game_data['directors'] if video_game_data['directors']
     end
 
     def set_producers(video_game, video_game_data)
-        video_game.producers = video_game_data['producers']
+        video_game.producers = video_game_data['producers'] if video_game_data['producers']
     end
 
     def set_designers(video_game, video_game_data)
-        video_game.designers = video_game_data['designers']
+        video_game.designers = video_game_data['designers'] if video_game_data['designers']
     end
 
     def set_programmers(video_game, video_game_data)
-        video_game.programmers = video_game_data['programmers']
+        video_game.programmers = video_game_data['programmers'] if video_game_data['programmers']
     end
 
     def set_artists(video_game, video_game_data)
-        video_game.artists = video_game_data['artists']
+        video_game.artists = video_game_data['artists'] if video_game_data['artists']
     end
 
     def set_composers(video_game, video_game_data)
-        video_game.composers = video_game_data['composers']
+        video_game.composers = video_game_data['composers'] if video_game_data['composers']
     end
 
     def set_platforms(video_game, video_game_data)
-        video_game.platforms = video_game_data['platforms']
+        video_game.platforms = video_game_data['platforms'] if video_game_data['platforms']
+    end
+
+    def set_date(video_game, video_game_data)
+        return unless video_game_data['date_released']
+
+        begin
+            video_game.date_released = Date.parse video_game_data['date_released']
+        rescue StandardError
+            return { fail_reason: "The provided date_released '#{video_game_data['date_released']}' is invalid." }
+        end
+
+        nil
     end
 end
