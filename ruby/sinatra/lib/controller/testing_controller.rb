d0f6fecf50f91935b8f23cdf1d4562e1f2a6f47b
Lars C. Finlay <larsfinlay@googlemail.com>
2021-04-13 11:05:27 +0100

    data_loader#load red

---
+++ ruby/sinatra/lib/controller/testing_controller.rb
@@ -0,0 +1,11 @@
+# handles api testing setup
+class TestingController
+    def initialize data_loader, storage
+        @data_loader = data_loader
+        @storage = storage
+    end
+
+    def setup
+        
+    end
+end
---
+++ ruby/sinatra/lib/services/data_loader.rb
@@ -0,0 +1,11 @@
+# loads and transforms all data resources into in memory objects
+class DataLoader
+    def initialize data_file_path, video_game_class
+        @data_file_path = data_file_path
+        @video_game_class = video_game_class
+    end
+
+    def load
+
+    end
+end
