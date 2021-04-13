fe2033a546f46246cbc9adf97b395f1ff7256231
Lars C. Finlay <larsfinlay@googlemail.com>
2021-04-13 11:40:52 +0100

    data_loader#load refactor

--- ruby/sinatra/lib/controller/testing_controller.rb
+++ ruby/sinatra/lib/controller/testing_controller.rb
@@ -1,11 +1,11 @@
+# frozen_string_literal: true
+
 # handles api testing setup
 class TestingController
-    def initialize data_loader, storage
+    def initialize(data_loader, storage)
         @data_loader = data_loader
         @storage = storage
     end
 
-    def setup
-        
-    end
+    def setup; end
 end
--- ruby/sinatra/spec/controller/testing_controller_spec.rb
+++ ruby/sinatra/spec/controller/testing_controller_spec.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
+
 require_relative '../../lib/controller/testing_controller'
 
 describe TestingController do
-
 end
