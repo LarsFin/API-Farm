74abe8dd8ca47d7d8f620ac7fd7682355e2f5251
Lars C. Finlay <larsfinlay@googlemail.com>
2021-04-13 10:03:59 +0100

    Environment Detection

--- ruby/sinatra/app.rb
+++ ruby/sinatra/app.rb
@@ -7,12 +7,22 @@ require_relative 'lib/models/video_game'
 require_relative 'lib/services/video_games'
 require_relative 'lib/storage/in_memory'
 
+# environments; DEV, TEST, PROD
+environment = ENV['RUBY_SINATRA_ENV'] || 'DEV'
+environment.upcase!
+
 storage = InMemory.new
 service = VideoGames.new storage, VideoGame
 controller = Controller.new service
 
 get '/' do
     'Hello World!'
+end
+
+if environment == 'TEST'
+    get '/api_tests/setup' do
+        'TBD'
+    end
 end
 
 get '/video_games' do
