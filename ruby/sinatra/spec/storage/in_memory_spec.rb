f2602d0bbbce79ef4756a0af850632062d1e369a
Karsten Finlay <KarstenFinlay@hotmail.com>
2021-04-14 11:38:59 +0100

    Stubbing of video game put after data constructions

--- ruby/sinatra/spec/storage/in_memory_spec.rb
+++ ruby/sinatra/spec/storage/in_memory_spec.rb
@@ -47,10 +47,11 @@ describe InMemory do
         it 'should get video game from array with passed index' do
             # Arrange
             video_game2 = double 'second video game', id: 2
-            allow(video_game2).to receive(:to_hash).and_return video_game2
             subject.video_games << double('first video game', id: 1)
             subject.video_games << video_game2
             subject.video_games << double('third video game', id: 3)
+
+            allow(video_game2).to receive(:to_hash).and_return video_game2
 
             # Act
             retrieved_video_game = subject.get 2
