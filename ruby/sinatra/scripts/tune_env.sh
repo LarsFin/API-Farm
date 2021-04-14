30633e6bf6352442e8c8711ae9c8acb625d2731c
Lars C. Finlay <larsfinlay@googlemail.com>
2021-04-14 12:31:37 +0100

    docker commands

---
+++ ruby/sinatra/scripts/tune_env.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# tunes necessary dependencies against the build environment
+
+# environments; dev, test, prod
+ENV=$1
+
+# if env is null; assume dev
+if [ -z $ENV ]
+    then ENV=dev
+fi
+
+# delete files unneeded to environments
+case $ENV in
+
+    dev)
+        echo "Dev environment selected!"
+        echo "No files to remove."
+        bundle install
+        ;;
+
+    test)
+        echo "Test environment selected!"
+        rm .rubocop.yml
+        bundle install --without dev
+        ;;
+
+    prod)
+        echo "Prod environment selected!"
+        rm .rubocop.yml
+        rm .rspec
+        rm -rf spec
+        bundle install --without dev test
+        ;;
+
+    *)
+        echo "Invalid environment selected!"
+        exit 1
+esac
