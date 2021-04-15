abc991b6718b57ac3cd37339aa76a7d786c7b392
Lars C. Finlay <larsfinlay@googlemail.com>
2021-04-15 14:55:52 +0100

    Build image removes pre-existing image

--- ruby/sinatra/scripts/build_img.sh
+++ ruby/sinatra/scripts/build_img.sh
@@ -12,6 +12,22 @@ if [ -z $ENV ]
         ENV=dev
 fi
 
+# check if image for environment already exists
+if [ "$ENV" == "prod" ]
+    then
+        if docker image inspect ruby/sinatra:prod >/dev/null 2>&1
+            then
+                echo "Image for ruby/sinatra prod already exists. Removing..."
+                docker rmi ruby/sinatra:prod
+        fi
+    else
+        if docker image inspect ruby/sinatra:dev >/dev/null 2>&1
+            then
+                echo "Image for ruby/sinatra dev already exists. Removing..."
+                docker rmi ruby/sinatra:dev
+        fi
+fi
+
 # begin docker build
 case $ENV in
 
