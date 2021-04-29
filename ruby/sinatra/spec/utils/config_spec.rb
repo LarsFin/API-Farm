93090f8dfdb182fc60cb6ae16763b0908d6a6992
Karsten Finlay <KarstenFinlay@hotmail.com>
2021-04-29 11:04:52 +0100

    Tested for if environment doesn't exist

--- ruby/sinatra/spec/utils/config_spec.rb
+++ ruby/sinatra/spec/utils/config_spec.rb
@@ -4,10 +4,10 @@ describe Config do
     environment = 'DEV'
     subject { Config.new environment}
     
-
     it 'should read and convert environment dev file' do
-        # Arrange
         
+
+        # Arrange
         dev_file = double 'DEV file path'
         config_item = double 'Data file resource'
         config_array = [config_item]
@@ -20,12 +20,25 @@ describe Config do
         # Assert
         expect(result).to be config_array
     end
+end
 
-    it 'should abort program with error message if environment does not exist' do
+describe Config do
+    environment = 'N/A'
+    subject { Config.new environment}
+    
+    it 'should read and convert environment dev file' do
+
         # Arrange
-
+        dev_file = double 'DEV file path'
+        config_item = double 'Data file resource'
+        config_array = [config_item]
+        allow(File).to receive(:read).with("config.dev.json").and_return dev_file
+        allow(JSON).to receive(:parse).with(dev_file).and_return config_array
+  
         # Act
+        result = subject.settings
 
         # Assert
+        expect(result).to eq "Given environment doesn't exist"
     end
 end
