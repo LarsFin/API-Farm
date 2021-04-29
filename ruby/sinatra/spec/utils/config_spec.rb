4997ca3664c603032b9843b5d8a31a125178f3ea
Karsten Finlay <KarstenFinlay@hotmail.com>
2021-04-29 10:59:56 +0100

    Test for reading file and hashing complete

--- ruby/sinatra/spec/utils/config_spec.rb
+++ ruby/sinatra/spec/utils/config_spec.rb
@@ -1,12 +1,31 @@
 require_relative '../../lib/utils/config'
 
 describe Config do
+    environment = 'DEV'
+    subject { Config.new environment}
+    
 
     it 'should read and convert environment dev file' do
         # Arrange
+        
+        dev_file = double 'DEV file path'
+        config_item = double 'Data file resource'
+        config_array = [config_item]
+        allow(File).to receive(:read).with("config.dev.json").and_return dev_file
+        allow(JSON).to receive(:parse).with(dev_file).and_return config_array
+  
+        # Act
+        result = subject.settings
 
         # Assert
+        expect(result).to be config_array
+    end
+
+    it 'should abort program with error message if environment does not exist' do
+        # Arrange
 
         # Act
+
+        # Assert
     end
 end
