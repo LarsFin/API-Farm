a394592cd595536d588ae49661e47ab8bde3c62d
Karsten Finlay <KarstenFinlay@hotmail.com>
2021-04-29 11:06:44 +0100

    Rubocop arrested

--- ruby/sinatra/lib/utils/config.rb
+++ ruby/sinatra/lib/utils/config.rb
@@ -1,25 +1,18 @@
+# frozen_string_literal: true
+
 require 'json'
 
+# loads and transforms all data resources into in settings hash
 class Config
     attr_reader :settings
 
-    def initialize environment
+    def initialize(environment)
+        if environment == 'DEV'
+            dev_file = File.read 'config.dev.json'
 
-        if environment == "DEV"
-            dev_file = File.read("config.dev.json")
-
-            @settings = JSON.parse(dev_file)
+            @settings = JSON.parse dev_file
         else
             abort "Given environment doesn't exist"
         end
-
-
-        # read respective config file. E.g; if DEV; should read from config file 'config.dev.json'. This file will be at the root of the lang/framework dir.
-        # convert read data into hash using json library
-        # set converted data to settings attribute
-        # unit tests too
-
-        # edge cases
-        # if a config file doesn't exist for used environment; throw error and stop program
     end
 end
--- ruby/sinatra/spec/utils/config_spec.rb
+++ ruby/sinatra/spec/utils/config_spec.rb
@@ -1,19 +1,19 @@
+# frozen_string_literal: true
+
 require_relative '../../lib/utils/config'
 
 describe Config do
     environment = 'DEV'
-    subject { Config.new environment}
-    
-    it 'should read and convert environment dev file' do
-        
+    subject { Config.new environment }
 
+    it 'should read and convert environment dev file' do
         # Arrange
         dev_file = double 'DEV file path'
         config_item = double 'Data file resource'
         config_array = [config_item]
-        allow(File).to receive(:read).with("config.dev.json").and_return dev_file
+        allow(File).to receive(:read).with('config.dev.json').and_return dev_file
         allow(JSON).to receive(:parse).with(dev_file).and_return config_array
-  
+
         # Act
         result = subject.settings
 
@@ -24,17 +24,16 @@ end
 
 describe Config do
     environment = 'N/A'
-    subject { Config.new environment}
-    
-    it 'should read and convert environment dev file' do
+    subject { Config.new environment }
 
+    it 'should read and convert environment dev file' do
         # Arrange
         dev_file = double 'DEV file path'
         config_item = double 'Data file resource'
         config_array = [config_item]
-        allow(File).to receive(:read).with("config.dev.json").and_return dev_file
+        allow(File).to receive(:read).with('config.dev.json').and_return dev_file
         allow(JSON).to receive(:parse).with(dev_file).and_return config_array
-  
+
         # Act
         result = subject.settings
 
