1056aa4dd21b428c99753153c2f2b043ce29f88f
Lars C. Finlay <larsfinlay@googlemail.com>
2021-05-04 14:12:18 +0100

    minimum coverage %

--- ruby/sinatra/spec/spec_helper.rb
+++ ruby/sinatra/spec/spec_helper.rb
@@ -7,4 +7,7 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
   SimpleCov::Formatter::Console
 ])
+
+SimpleCov.minimum_coverage 100
+
 SimpleCov.start
