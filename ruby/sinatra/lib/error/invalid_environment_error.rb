d9bd46839cc69af4bce9339514f3c7682974c985
Karsten Finlay <KarstenFinlay@hotmail.com>
2021-05-04 11:16:40 +0100

    Got rid of env attribute

--- ruby/sinatra/lib/error/invalid_environment_error.rb
+++ ruby/sinatra/lib/error/invalid_environment_error.rb
@@ -2,10 +2,8 @@
 
 # Unique error message for when an environment doesn't exist
 class InvalidEnvironmentError < StandardError
-    attr_reader :environment
 
     def initialize(environment)
-        @environment = environment
         super "Given environment #{environment} doesn't exist"
     end
 end
