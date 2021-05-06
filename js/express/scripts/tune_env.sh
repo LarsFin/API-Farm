fd5eb628741622b978b3aaf644d332fba65c8d91
KarstenFinlay <KarstenFinlay@hotmail.com>
2021-05-06 15:45:42 +0100

    Added tune_env.sh script

---
+++ js/express/scripts/tune_env.sh
@@ -0,0 +1,29 @@
+# tunes necessary dependencies against the build environment
+
+# environments; dev, test, prod
+ENV=$1
+
+# if env is null; assume dev
+if [ -z $ENV ]
+  then ENV=dev
+fi
+
+# delete files unneeded to environments
+case $ENV in
+
+  dev)
+    echo "Dev environment selected"
+    echo "No files to remove."
+    npm install
+    ;;
+  
+  prod)
+    echo "Prod environment selected"
+    rm .eslintrc.json
+    npm install --no-optional
+    ;;
+    
+  *)
+    echo "Invalid environment selected!"
+    exit 1
+esac
