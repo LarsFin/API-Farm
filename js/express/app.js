6f04993780bcbf3651612e437c6cdb94c9d404b3
LarsFin <larsfinlay@googlemail.com>
2021-05-08 16:12:07 +0100

    app#getAll

--- js/express/app.js
+++ js/express/app.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/ping', (req, res) => {
+const Controller = require('./lib/controllers/controller');
+const VideoGamesService = require('./lib/services/video-games');
+const InMemory = require('./lib/storage/in-memory');
+
+const storage = new InMemory();
+const videoGamesService = new VideoGamesService(storage);
+const controller = new Controller(videoGamesService);
+
+app.get('/ping', (_, res) => {
     res.send('pong');
+});
+
+app.get('/video_games', (_, res) => {
+    controller.getAll(res);
 });
 
 app.listen(port, () => {
--- js/express/lib/controllers/controller.js
+++ js/express/lib/controllers/controller.js
@@ -1,9 +1,9 @@
 function Controller (videoGamesService) {
-    this.videoGamesService = videoGamesService;
+    this._videoGamesService = videoGamesService;
 }
 
 Controller.prototype.getAll = function (res) {
-    const videoGames = this.videoGamesService.getAll();
+    const videoGames = this._videoGamesService.getAll();
 
     res.status(200);
     res.header('Content-Type', 'application/json');
