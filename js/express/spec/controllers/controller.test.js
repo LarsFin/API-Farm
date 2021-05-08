6bcd5cf7a684bec919f9dd827f04ddf8e38cc860
LarsFin <larsfinlay@googlemail.com>
2021-05-08 14:51:06 +0100

    controller#getAll red

---
+++ js/express/spec/controllers/controller.test.js
@@ -0,0 +1,32 @@
+const Controller = require('../../lib/controllers/controller');
+
+let controller;
+let mockVideoGamesService;
+
+beforeEach(() => {
+    mockVideoGamesService = {};
+    controller = new Controller(mockVideoGamesService);
+});
+
+test("getAll should retrieve video games from service and respond 200", () => {
+    // Arrange
+    let mockRes = {};
+    mockRes.status = jest.fn();
+    mockRes.header = jest.fn();
+    mockRes.json = jest.fn();
+    let videoGames = [];
+    mockVideoGamesService.getAll = jest.fn(() => videoGames);
+
+    // Act
+    controller.getAll(mockRes);
+
+    // Assert
+    expect(mockRes.status).toHaveBeenCalledTimes(1);
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+
+    expect(mockRes.header).toHaveBeenCalledTimes(1);
+    expect(mockRes.header).toHaveBeenCalledWith('Content-Type', 'application/json');
+
+    expect(mockRes.json).toHaveBeenCalledTimes(1);
+    expect(mockRes.json).toHaveBeenCalledWith(videoGames);
+});
