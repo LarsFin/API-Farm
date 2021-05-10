834ceff27f298f0cb21e69f0d7cfb7c0579f4535
LarsFin <larsfinlay@googlemail.com>
2021-05-10 12:13:54 +0100

    app#post

--- js/express/lib/extensions/response.js
+++ js/express/lib/extensions/response.js
@@ -7,3 +7,17 @@ express.response.ok = function (body) {
     this.header('Content-Type', 'application/json');
     this.json(body);
 };
+
+express.response.created = function (body) {
+    this.status(201);
+    this.header('Content-Type', 'application/json');
+    this.json(body);
+};
+
+// 4** Responses
+
+express.response.badRequest = function (reason) {
+    this.status(400);
+    this.header('Content-Type', 'text/plain');
+    this.send(reason);
+};
