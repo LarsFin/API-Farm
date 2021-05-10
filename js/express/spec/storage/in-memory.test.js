b8c414930729740b860c10bc3466a01ddabe8560
LarsFin <larsfinlay@googlemail.com>
2021-05-10 11:16:11 +0100

    in-memory#addVideoGame red->green

--- js/express/spec/storage/in-memory.test.js
+++ js/express/spec/storage/in-memory.test.js
@@ -1,12 +1,32 @@
 const InMemory = require('../../lib/storage/in-memory');
 
-test('getAllVideoGames should return internal video games array', () => {
-    // Arrange
-    const inMemory = new InMemory();
+let inMemory;
 
+beforeEach(() => {
+    inMemory = new InMemory();
+});
+
+// Get All Video Games
+
+test('getAllVideoGames should return internal video games array', () => {
     // Act
     const videoGames = inMemory.getAllVideoGames();
 
     // Assert
     expect(videoGames).toBe(inMemory._videoGames);
+});
+
+// Add Video Game
+
+test('addVideoGame should increment and set id, then push to video games array', () => {
+    // Arrange
+    const videoGame = {};
+
+    // Act
+    const storedVideoGame = inMemory.addVideoGame(videoGame);
+
+    // Assert
+    expect(inMemory._videoGames.length).toBe(1);
+    expect(inMemory._videoGames[0]).toBe(videoGame);
+    expect(storedVideoGame.id).toBe(1);
 });
