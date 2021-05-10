f3ce05e05c6603a338680a4e10006f38e0c5099a
LarsFin <larsfinlay@googlemail.com>
2021-05-10 10:46:40 +0100

    videoGames#add red->green

--- js/express/lib/services/video-games.js
+++ js/express/lib/services/video-games.js
@@ -4,10 +4,67 @@ function VideoGamesService (storage) {
     this._storage = storage;
 }
 
+const newVideoGame = (name, date_released) => {
+    return {
+        name,
+        developers: [],
+        publishers: [],
+        directors: [],
+        producers: [],
+        designers: [],
+        programmers: [],
+        artists: [],
+        composers: [],
+        platforms: [],
+        date_released
+    }
+};
+
+// date should always be in format DD/MM/YYYY
+const isValidDate = date_released => {
+    dateElements = date_released.split('/');
+
+    if (dateElements.length !== 3)
+        return false;
+
+    return !isNaN(new Date(`${dateElements[1]}/${dateElements[0]}/${dateElements[2]}`));
+};
+
 VideoGamesService.prototype.getAll = function () {
     const videoGames = this._storage.getAllVideoGames();
 
     return Query.success(videoGames);
+};
+
+VideoGamesService.prototype.add = function (data) {
+    const videoGameName = data['name'];
+
+    if (!videoGameName)
+        return Query.fail(400, 'A name is required for a video game.');
+
+    const videoGameDateReleased = data['date_released'];
+
+    if (!videoGameDateReleased)
+        return Query.fail(400, 'A date_released is required for a video game.');
+
+    if (!isValidDate(videoGameDateReleased))
+        return Query.fail(400, `The provided date_released '${videoGameDateReleased}' is invalid.`);
+
+    let videoGame = newVideoGame(videoGameName, videoGameDateReleased);
+
+    for (const [key, value] of Object.entries(data)) {
+        if (['name', 'date_released'].includes(key))
+            continue;
+
+        if (!videoGame.hasOwnProperty(key))
+            return Query.fail(400, `The provided data has an invalid attribute '${key}'.`);
+        
+        videoGame[key] = value;
+    }
+
+    videoGame = this._storage.add(videoGame);
+
+    return Query.success(videoGame);
 };
 
 module.exports = VideoGamesService;
--- js/express/spec/services/video-games.test.js
+++ js/express/spec/services/video-games.test.js
@@ -10,6 +10,8 @@ beforeEach(() => {
     videoGamesService = new VideoGamesService(mockStorage);
 });
 
+// Get All
+
 test('getAll should query storage and return video games', () => {
     // Arrange
     const videoGames = [];
@@ -25,4 +27,221 @@ test('getAll should query storage and return video games', () => {
     expect(Query.success).toHaveBeenCalledWith(videoGames);
 
     expect(query).toBe(successfulQuery);
+});
+
+// Add
+
+test('add should instance a video game object and add to storage', () => {
+    // Arrange
+    const name = 'Video Game Quest II';
+    const developers = ['VG DEVS'];
+    const publishers = ['VG PUBS'];
+    const directors = ['VG DIR'];
+    const artists = ['VG ARTISTS'];
+    const composers = ['VG COMPS'];
+    const platforms = ['PLATFORM X'];
+    const date_released = '15/04/2004';
+    const data = {
+        name,
+        developers,
+        publishers,
+        directors,
+        artists,
+        composers,
+        platforms,
+        date_released
+    };
+    const storedVideoGame = {};
+    mockStorage.add = jest.fn(() => storedVideoGame);
+    const successfulQuery = {};
+    Query.success = jest.fn(() => successfulQuery);
+
+    // Act
+    const query = videoGamesService.add(data);
+
+    // Assert
+    expect(mockStorage.add).toHaveBeenCalledTimes(1);
+    expect(mockStorage.add).toHaveBeenCalledWith(
+        expect.objectContaining({
+            name,
+            developers,
+            publishers,
+            directors,
+            producers: [],
+            designers: [],
+            programmers: [],
+            artists,
+            composers,
+            platforms,
+            date_released
+        })
+    );
+
+    expect(Query.success).toHaveBeenCalledTimes(1);
+    expect(Query.success).toHaveBeenCalledWith(storedVideoGame);
+
+    expect(query).toBe(successfulQuery);
+});
+
+test('add should fail when data has no name', () => {
+    // Arrange
+    const developers = ['VG DEVS'];
+    const publishers = ['VG PUBS'];
+    const directors = ['VG DIR'];
+    const artists = ['VG ARTISTS'];
+    const composers = ['VG COMPS'];
+    const platforms = ['PLATFORM X'];
+    const date_released = '15/04/2004';
+    const data = {
+        developers,
+        publishers,
+        directors,
+        artists,
+        composers,
+        platforms,
+        date_released
+    };
+    const failedQuery = {};
+    Query.fail = jest.fn(() => failedQuery);
+
+    // Act
+    const query = videoGamesService.add(data);
+
+    // Assert
+    expect(Query.fail).toHaveBeenCalledTimes(1);
+    expect(Query.fail).toHaveBeenCalledWith(400, 'A name is required for a video game.');
+
+    expect(query).toBe(failedQuery);
+});
+
+test('add should fail when data has no date_released', () => {
+    // Arrange
+    const name = 'Video Game Quest II';
+    const developers = ['VG DEVS'];
+    const publishers = ['VG PUBS'];
+    const directors = ['VG DIR'];
+    const artists = ['VG ARTISTS'];
+    const composers = ['VG COMPS'];
+    const platforms = ['PLATFORM X'];
+    const data = {
+        name,
+        developers,
+        publishers,
+        directors,
+        artists,
+        composers,
+        platforms
+    };
+    const failedQuery = {};
+    Query.fail = jest.fn(() => failedQuery);
+
+    // Act
+    const query = videoGamesService.add(data);
+
+    // Assert
+    expect(Query.fail).toHaveBeenCalledTimes(1);
+    expect(Query.fail).toHaveBeenCalledWith(400, 'A date_released is required for a video game.');
+
+    expect(query).toBe(failedQuery);
+});
+
+test('add should fail when data has an invalid date_released (1)', () => {
+    // Arrange
+    const name = 'Video Game Quest II';
+    const developers = ['VG DEVS'];
+    const publishers = ['VG PUBS'];
+    const directors = ['VG DIR'];
+    const artists = ['VG ARTISTS'];
+    const composers = ['VG COMPS'];
+    const platforms = ['PLATFORM X'];
+    const date_released = '04/15/2004';
+    const data = {
+        name,
+        developers,
+        publishers,
+        directors,
+        artists,
+        composers,
+        platforms,
+        date_released
+    };
+    const failedQuery = {};
+    Query.fail = jest.fn(() => failedQuery);
+
+    // Act
+    const query = videoGamesService.add(data);
+
+    // Assert
+    expect(Query.fail).toHaveBeenCalledTimes(1);
+    expect(Query.fail).toHaveBeenCalledWith(400, `The provided date_released '${date_released}' is invalid.`);
+
+    expect(query).toBe(failedQuery);
+});
+
+test('add should fail when data has an invalid date_released (2)', () => {
+    // Arrange
+    const name = 'Video Game Quest II';
+    const developers = ['VG DEVS'];
+    const publishers = ['VG PUBS'];
+    const directors = ['VG DIR'];
+    const artists = ['VG ARTISTS'];
+    const composers = ['VG COMPS'];
+    const platforms = ['PLATFORM X'];
+    const date_released = 'Last Monday';
+    const data = {
+        name,
+        developers,
+        publishers,
+        directors,
+        artists,
+        composers,
+        platforms,
+        date_released
+    };
+    const failedQuery = {};
+    Query.fail = jest.fn(() => failedQuery);
+
+    // Act
+    const query = videoGamesService.add(data);
+
+    // Assert
+    expect(Query.fail).toHaveBeenCalledTimes(1);
+    expect(Query.fail).toHaveBeenCalledWith(400, `The provided date_released '${date_released}' is invalid.`);
+
+    expect(query).toBe(failedQuery);
+});
+
+test('add should fail when data has an invalid attribute', () => {
+    // Arrange
+    const name = 'Video Game Quest II';
+    const developers = ['VG DEVS'];
+    const publishers = ['VG PUBS'];
+    const directors = ['VG DIR'];
+    const artists = ['VG ARTISTS'];
+    const composers = ['VG COMPS'];
+    const platforms = ['PLATFORM X'];
+    const testers = ['VG TESTERS'];
+    const date_released = '15/04/2004';
+    const data = {
+        name,
+        developers,
+        publishers,
+        directors,
+        artists,
+        composers,
+        platforms,
+        testers,
+        date_released
+    };
+    const failedQuery = {};
+    Query.fail = jest.fn(() => failedQuery);
+
+    // Act
+    const query = videoGamesService.add(data);
+
+    // Assert
+    expect(Query.fail).toHaveBeenCalledTimes(1);
+    expect(Query.fail).toHaveBeenCalledWith(400, 'The provided data has an invalid attribute \'testers\'.');
+
+    expect(query).toBe(failedQuery);
 });
