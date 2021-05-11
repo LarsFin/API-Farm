11af74055bf6a0aafc1ebeee97e7a7e88f13be94
LarsFin <larsfinlay@googlemail.com>
2021-05-11 09:03:10 +0100

    in-memory#getVideoGame red->green

--- js/express/lib/storage/in-memory.js
+++ js/express/lib/storage/in-memory.js
@@ -3,6 +3,15 @@ function InMemory () {
     this._videoGames = [];
 }
 
+InMemory.prototype.getVideoGame = function (id) {
+    for (const videoGame of this._videoGames) {
+        if (videoGame.id === id)
+            return videoGame;
+    }
+
+    return;
+};
+
 InMemory.prototype.getAllVideoGames = function () {
     return this._videoGames;
 };
--- js/express/spec/storage/in-memory.test.js
+++ js/express/spec/storage/in-memory.test.js
@@ -6,6 +6,30 @@ beforeEach(() => {
     inMemory = new InMemory();
 });
 
+// Get Video Game
+
+test('getVideoGame should return video game with specified id', () => {
+    // Arrange
+    const videoGame1 = { id: 1 };
+    const videoGame2 = { id: 3 };
+    const videoGame3 = { id: 4 };
+    inMemory._videoGames = [videoGame1, videoGame2, videoGame3];
+
+    // Act
+    const videoGame = inMemory.getVideoGame(3);
+
+    // Assert
+    expect(videoGame).toBe(videoGame2);
+});
+
+test('getVideoGame should return null when the video game with id could not be found', () => {
+    // Act
+    const videoGame = inMemory.getVideoGame(5);
+
+    // Assert
+    expect(videoGame).toBe(undefined);
+});
+
 // Get All Video Games
 
 test('getAllVideoGames should return internal video games array', () => {
