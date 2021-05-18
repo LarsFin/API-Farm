6bb24039774b8e97800f3fe8792c27f434fd6b78
Lars C. Finlay <larsfinlay@googlemail.com>
2021-05-18 10:16:22 +0100

    C# init

---
+++ csharp/aspnet/ApiFarm/Controllers/VideoGamesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiFarm.Controllers
+{
+    [Route("video_games")]
+    [ApiController]
+    public class VideoGamesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello World!";
+        }
+    }
+}
