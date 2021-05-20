d7e9b49dff082e491d8643bf9a32bdbc7f9d4c56
Karsten Finlay <KarstenFinlay@hotmail.com>
2021-05-20 16:45:59 +0100

    Linting now ignores test files

--- python/flask/app.py
+++ python/flask/app.py
@@ -1,9 +1,13 @@
-from flask import Flask 
+"""
+TODO.
+"""
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    """This is a basic start."""
     return "Hello, Docker!"
 
 if __name__ == '__main__':
