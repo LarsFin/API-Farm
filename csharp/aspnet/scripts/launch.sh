adec74debc144eeb010ec89f9c7873d2f50f14a9
Lars C. Finlay <larsfinlay@googlemail.com>
2021-05-20 15:42:33 +0100

    configured docker with build stages

---
+++ csharp/aspnet/scripts/launch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# commands; run, test, lint
+CMD=$1
+
+# if cmd is null; assume run
+if [ -z $CMD ]
+  then CMD=run
+fi
+
+# run process correlating to command
+case $CMD in
+
+  run)
+    echo "Running app..."
+    dotnet run ApiFarm
+    ;;
+
+  test)
+    echo "Running test suite..."
+    dotnet test /p:Exclude=\"[*]ApiFarm.Scaffolding.*,[*]ApiFarm.Models.*\" /p:CollectCoverage=true /p:Threshold=100
+    ;;
+
+  lint)
+    echo "Linting codebase..."
+    dotnet build /warnaserror
+    ;;
+  
+  *) 
+    echo "Unknown command given; ${CMD}"
+    exit 1
+esac
