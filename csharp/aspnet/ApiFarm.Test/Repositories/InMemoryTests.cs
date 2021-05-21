f188e0a50a06cea56d9828483d15a0429400d0fe
Lars C. Finlay <larsfinlay@googlemail.com>
2021-05-21 15:16:24 +0100

    InMemory::GetAll red

---
+++ csharp/aspnet/ApiFarm.Test/Repositories/InMemoryTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using ApiFarm.Models;
+using ApiFarm.Repositories;
+using NUnit.Framework;
+using Shouldly;
+
+namespace ApiFarm.Test.Repositories
+{
+    public class InMemoryTests
+    {
+        private InMemory<Model> subject;
+
+        private List<Model> models;
+
+        [SetUp]
+        public void Setup()
+        {
+            models = new List<Model>();
+            subject = new InMemory<Model>(models);
+        }
+
+        private class GetAllShould : InMemoryTests
+        {
+            [Test]
+            public void ReturnModelsList()
+            {
+                // Act
+                var retrievedModels = subject.GetAll();
+
+                // Assert
+                retrievedModels.ShouldBe(models);
+            }
+        }
+
+        private class Model : IModel
+        {
+            public uint Id { get; set; }
+        }
+    }
+}
