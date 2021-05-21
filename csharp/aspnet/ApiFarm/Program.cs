0234ef589872a1064bbebe8b46f3ea81faf8c992
Lars C. Finlay <larsfinlay@googlemail.com>
2021-05-21 10:09:01 +0100

    quick fix for open bind on api

--- csharp/aspnet/ApiFarm/Program.cs
+++ csharp/aspnet/ApiFarm/Program.cs
@@ -13,6 +13,7 @@ namespace ApiFarm.Scaffolding
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:8080")
                 .UseStartup<Startup>();
     }
 }
