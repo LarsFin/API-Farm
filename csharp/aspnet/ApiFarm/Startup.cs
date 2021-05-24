0e7106ca9fa19d0cee91eeea615b0158485b1ce2
Lars C. Finlay <larsfinlay@googlemail.com>
2021-05-24 11:08:20 +0100

    added services to complete get all pipeline

--- csharp/aspnet/ApiFarm/Startup.cs
+++ csharp/aspnet/ApiFarm/Startup.cs
@@ -1,4 +1,10 @@
 ﻿#pragma warning disable
+using ApiFarm.Models.Impl;
+using ApiFarm.Repositories;
+using ApiFarm.Services;
+using ApiFarm.Services.Impl;
+using ApiFarm.Utils;
+using ApiFarm.Utils.Impl;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -19,6 +25,12 @@ namespace ApiFarm.Scaffolding
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddTransient<IRepository<VideoGame>, InMemory<VideoGame>>();
+
+            services.AddTransient<IService<VideoGame>, VideoGameService>();
+
+            services.AddTransient<IQueryFactory, QueryFactory>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
