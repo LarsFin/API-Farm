fd3da58ca60b99cef1883ee770064a1fa1f93242
Karsten Finlay <KarstenFinlay@hotmail.com>
2021-05-25 11:01:06 +0100

    InMemory created

--- python/flask/app.py
+++ python/flask/app.py
@@ -5,10 +5,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/ping")
 def hello_world():
     """This is a basic start."""
-    return "Hello, Docker!"
+    return "pong"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
---
+++ python/flask/lib/storage/in_memory.py
@@ -0,0 +1,4 @@
+''' in memory storage '''
+class InMemory:
+    ''' In memory '''
+    video_games = []
