8000907e83548bbe961c54e0855954cc22d95e9e
Karsten Finlay <KarstenFinlay@hotmail.com>
2021-05-26 15:28:08 +0100

    in_memory get red

--- python/flask/lib/storage/in_memory.py
+++ python/flask/lib/storage/in_memory.py
@@ -8,4 +8,8 @@ class InMemory():
     def add(self, video_game):
         '''Adds a video game object to the video games array'''
         self.video_games.append(video_game)
+
+    def get(self, index):
+        '''Gets a video game object from the video games list'''
+        
         
--- python/flask/spec/storage/in_memory_test.py
+++ python/flask/spec/storage/in_memory_test.py
@@ -2,7 +2,6 @@ from flask.lib.storage.in_memory import InMemory
 from unittest.mock import Mock
 
 def test_should_add_game_to_array():
-    print("Are we here?")
     # Arrange
     mock = Mock()
     video_game = mock
@@ -13,7 +12,20 @@ def test_should_add_game_to_array():
     in_memory.add(video_game)
     
     # Assert
-    print("We are in assert")
-    print(len(video_games))
     assert len(video_games) == 1
     assert in_memory.video_games == [video_game]
+
+def test_should_get_video_game_from_array():
+    # Arrange
+    mock = Mock()
+    video_game_1 = mock
+    video_game_2 = mock
+    video_game_3 = mock
+    video_games = [video_game_1, video_game_2, video_game_3]
+    in_memory = InMemory(video_games)
+
+    # Act
+    retrieved_video_game = in_memory.get(2)
+
+    # Assert
+    assert retrieved_video_game == video_game_2
