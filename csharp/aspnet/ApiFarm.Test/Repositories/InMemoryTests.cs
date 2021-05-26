087a29d89cd5694b4d0fbefb0339df3e1380c751
Lars C. Finlay <larsfinlay@googlemail.com>
2021-05-26 16:58:47 +0100

    amended in memory functionality

--- csharp/aspnet/ApiFarm.Test/Repositories/InMemoryTests.cs
+++ csharp/aspnet/ApiFarm.Test/Repositories/InMemoryTests.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using ApiFarm.Models;
 using ApiFarm.Repositories;
+using ApiFarm.Utils;
+using Moq;
 using NUnit.Framework;
 using Shouldly;
 
@@ -10,13 +12,20 @@ namespace ApiFarm.Test.Repositories
     {
         private InMemory<Model> subject;
 
+        private Mock<ICloner<Model>> mockCloner;
         private List<Model> models;
 
         [SetUp]
         public void Setup()
         {
+            mockCloner = new Mock<ICloner<Model>>();
             models = new List<Model>();
-            subject = new InMemory<Model>(models);
+            subject = new InMemory<Model>(mockCloner.Object, models);
+        }
+
+        public class Model : IModel
+        {
+            public uint Id { get; set; }
         }
 
         private class GetAllShould : InMemoryTests
@@ -25,30 +34,32 @@ namespace ApiFarm.Test.Repositories
             public void ReturnEmptyModelsList()
             {
                 // Arrange
-                subject = new InMemory<Model>();
+                subject = new InMemory<Model>(mockCloner.Object);
 
                 // Act
                 var retrievedModels = subject.GetAll();
 
                 // Assert
-                retrievedModels.ShouldBeOfType<List<Model>>();
                 retrievedModels.ShouldBeEmpty();
             }
 
             [Test]
-            public void ReturnStubbedModelsList()
+            public void ReturnListOfClonedModels()
             {
+                // Arrange
+                var originalModel = new Model();
+                models.Add(originalModel);
+                var cloneModel = new Model();
+
+                mockCloner.Setup(m => m.Clone(originalModel)).Returns(cloneModel);
+
                 // Act
                 var retrievedModels = subject.GetAll();
 
                 // Assert
-                retrievedModels.ShouldBe(models);
+                retrievedModels.ShouldNotBeSameAs(models);
+                retrievedModels.ShouldContain(cloneModel);
             }
-        }
-
-        private class Model : IModel
-        {
-            public uint Id { get; set; }
         }
     }
 }
--- csharp/aspnet/ApiFarm/Repositories/Impl/InMemory.cs
+++ csharp/aspnet/ApiFarm/Repositories/Impl/InMemory.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using ApiFarm.Models;
+using ApiFarm.Utils;
 
 [assembly: InternalsVisibleTo("ApiFarm.Test")]
 
@@ -13,18 +15,22 @@ namespace ApiFarm.Repositories
     public class InMemory<T> : IRepository<T>
          where T : IModel
     {
+        private ICloner<T> cloner;
         private List<T> videoGames;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="InMemory{T}"/> class.
         /// </summary>
-        public InMemory()
+        /// <param name="cloner">Responsible for cloning stored entities to avoid external storage manipulation.</param>
+        public InMemory(ICloner<T> cloner)
         {
+            this.cloner = cloner;
             this.videoGames = new List<T>();
         }
 
-        internal InMemory(List<T> presetVideoGames)
+        internal InMemory(ICloner<T> cloner, List<T> presetVideoGames)
         {
+            this.cloner = cloner;
             this.videoGames = presetVideoGames;
         }
 
@@ -32,6 +38,9 @@ namespace ApiFarm.Repositories
         /// Returns private list of stored entities.
         /// </summary>
         /// <returns>List of stored entities.</returns>
-        public IEnumerable<T> GetAll() => this.videoGames;
+        public IEnumerable<T> GetAll()
+        {
+            return this.videoGames.Select(q => this.cloner.Clone(q));
+        }
     }
 }
