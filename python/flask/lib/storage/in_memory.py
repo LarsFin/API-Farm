7c9f231f2ed70b52e2319821e2f363abfb640b8b
Karsten Finlay <KarstenFinlay@hotmail.com>
2021-05-26 14:51:08 +0100

    in_memory add refactor

--- python/flask/lib/storage/in_memory.py
+++ python/flask/lib/storage/in_memory.py
@@ -1,8 +1,11 @@
-''' in memory storage '''
+'''TODO'''
 class InMemory():
+    '''in memory storage'''
     def __init__(self, video_games):
+        '''init method'''
         self.video_games = video_games
 
     def add(self, video_game):
+        '''Adds a video game object to the video games array'''
         self.video_games.append(video_game)
         
