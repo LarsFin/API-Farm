b8f863db765364937bb41b571828b54a18fe544a
Lars C. Finlay <larsfinlay@googlemail.com>
2021-06-16 16:20:54 +0100

    Controller::Post (400) refactor

--- go/mux/src/response.go
+++ go/mux/src/response.go
@@ -22,43 +22,35 @@ func NewHTTPResponse(w *http.ResponseWriter) *HTTPResponse {
 	}
 }
 
-func (r *HTTPResponse) OkJSON(data []byte) {
-	_, err := (*r.w).Write(data)
+func write(w *http.ResponseWriter, data []byte) {
+	_, err := (*w).Write(data)
 
 	if err != nil {
-		http.Error(*r.w, err.Error(), http.StatusInternalServerError)
+		http.Error(*w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func (r *HTTPResponse) OkJSON(data []byte) {
+	write(r.w, data)
 
 	(*r.w).Header().Set("Content-Type", "application/json")
 }
 
 func (r *HTTPResponse) OkText(text string) {
-	_, err := (*r.w).Write([]byte(text))
-
-	if err != nil {
-		http.Error(*r.w, err.Error(), http.StatusInternalServerError)
-	}
+	write(r.w, []byte(text))
 
 	(*r.w).Header().Set("Content-Type", "text/plain")
 }
 
 func (r *HTTPResponse) CreatedJSON(data []byte) {
-	_, err := (*r.w).Write(data)
-
-	if err != nil {
-		http.Error(*r.w, err.Error(), http.StatusInternalServerError)
-	}
+	write(r.w, data)
 
 	(*r.w).WriteHeader(http.StatusCreated)
 	(*r.w).Header().Set("Content-Type", "application/json")
 }
 
 func (r *HTTPResponse) BadRequestText(text string) {
-	_, err := (*r.w).Write([]byte(text))
-
-	if err != nil {
-		http.Error(*r.w, err.Error(), http.StatusInternalServerError)
-	}
+	write(r.w, []byte(text))
 
 	(*r.w).WriteHeader(http.StatusBadRequest)
 	(*r.w).Header().Set("Content-Type", "text/plain")
