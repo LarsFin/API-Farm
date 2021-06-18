70a1c76b590fafd2e27eb433dc0f4207b52e0ba2
Lars C. Finlay <larsfinlay@googlemail.com>
2021-06-18 11:15:34 +0100

    DataLoader::Load (fr fail) red

--- go/mux/src/dataloader_test.go
+++ go/mux/src/dataloader_test.go
@@ -3,6 +3,7 @@ package apifarm_test
 import (
 	mocks "apifarm/mocks/src"
 	apifarm "apifarm/src"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -31,6 +32,31 @@ func TestJSONFileLoaderLoadSuccessful(t *testing.T) {
 	mockStorage.On("AddVideoGame", vg2).Return(apifarm.VideoGame{})
 	mockStorage.On("AddVideoGame", vg3).Return(apifarm.VideoGame{})
 	mockQueryFactory.On("BuildMessage", apifarm.SuccessfullyLoadedData, uint(0)).Return(expectedQuery)
+
+	// Act
+	actualQuery := subject.Load(path)
+
+	// Assert
+	assert.Equal(t, expectedQuery, actualQuery)
+	mockJSON.AssertExpectations(t)
+	mockFileUtils.AssertExpectations(t)
+}
+
+func TestJSONFileLoaderLoadFileReadFailure(t *testing.T) {
+	// Arrange
+	mockStorage := new(mocks.DB)
+	mockJSON := new(mocks.DataUtils)
+	mockFileUtils := new(mocks.FileUtils)
+	mockQueryFactory := new(mocks.QueryFactory)
+
+	subject := apifarm.NewJSONFileLoaderWithUtils(mockStorage, mockJSON, mockFileUtils, mockQueryFactory)
+
+	path := "PATH TO DATA FILE"
+	err := errors.New("failed to read file")
+	expectedQuery := apifarm.Query{}
+
+	mockFileUtils.On("Read", path).Return(nil, err)
+	mockQueryFactory.On("Error", err).Return(expectedQuery)
 
 	// Act
 	actualQuery := subject.Load(path)
