9892737bc231cf388c42baeb168489b0025b5267
Lars C. Finlay <larsfinlay@googlemail.com>
2021-06-22 15:51:42 +0100

    GetConfiguration (read fail) red

--- go/mux/src/configuration_test.go
+++ go/mux/src/configuration_test.go
@@ -3,12 +3,13 @@ package apifarm_test
 import (
 	mocks "apifarm/mocks/src"
 	apifarm "apifarm/src"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func Test(t *testing.T) {
+func TestGetConfiguration(t *testing.T) {
 	// Arrange
 	mockJSON := new(mocks.DataUtils)
 	mockFileUtils := new(mocks.FileUtils)
@@ -26,4 +27,23 @@ func Test(t *testing.T) {
 	// Assert
 	assert.Equal(t, expected, got)
 	assert.Nil(t, err)
+}
+
+func TestGetConfigurationReadFailure(t *testing.T) {
+	// Arrange
+	mockJSON := new(mocks.DataUtils)
+	mockFileUtils := new(mocks.FileUtils)
+
+	path := "PATH TO CONFIG FILE"
+	expectedErr := errors.New("could not find file")
+	expected := apifarm.Configuration{}
+
+	mockFileUtils.On("Read", path).Return(nil, expectedErr)
+
+	// Act
+	got, gotErr := apifarm.GetConfiguration(path, mockJSON, mockFileUtils)
+
+	// Assert
+	assert.Equal(t, expected, got)
+	assert.Equal(t, expectedErr, gotErr)
 }
