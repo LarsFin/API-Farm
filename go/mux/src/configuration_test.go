b4a1b70a23da6f8c19d7840d42400f1db33636df
Lars C. Finlay <larsfinlay@googlemail.com>
2021-06-22 16:35:45 +0100

    GetConfiguration (deserialize fail) red

--- go/mux/src/configuration_test.go
+++ go/mux/src/configuration_test.go
@@ -47,3 +47,24 @@ func TestGetConfigurationReadFailure(t *testing.T) {
 	assert.Equal(t, expected, got)
 	assert.Equal(t, expectedErr, gotErr)
 }
+
+func TestGetConfigurationDeserializeFailure(t *testing.T) {
+	// Arrange
+	mockJSON := new(mocks.DataUtils)
+	mockFileUtils := new(mocks.FileUtils)
+
+	path := "PATH TO CONFIG FILE"
+	configData := []byte{24, 66, 80}
+	expectedErr := errors.New("failed to deserialize")
+	expected := apifarm.Configuration{}
+
+	mockFileUtils.On("Read", path).Return(configData, nil)
+	mockJSON.On("DeserializeConfiguration", configData).Return(&expected, expectedErr)
+
+	// Act
+	got, gotErr := apifarm.GetConfiguration(path, mockJSON, mockFileUtils)
+
+	// Assert
+	assert.Equal(t, expected, got)
+	assert.Equal(t, expectedErr, gotErr)
+}
